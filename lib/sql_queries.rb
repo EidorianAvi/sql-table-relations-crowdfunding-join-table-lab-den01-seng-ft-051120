f7dffa3b3e6da0e13e1b8e002d08277b6ac4383e
Rene Avila <reneavila1993@gmail.com>
2020-05-21 19:26:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -45,7 +45,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
    FROM Projects
    INNER JOIN Pledges 
    ON Projects.id = Pledges.project_id
-   GROUP BY Projects.category"
+   GROUP BY category"
    
 end
 
