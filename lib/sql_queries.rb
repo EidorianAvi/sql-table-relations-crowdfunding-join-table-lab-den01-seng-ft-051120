06bd7a7d0e04ede280351bee7e9f51cb9ebfccd6
Rene Avila <reneavila1993@gmail.com>
2020-05-21 19:25:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,8 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT Projects.category, SUM(Pledge.amount) WHERE Pledges.category = 'music'
    FROM Projects
    INNER JOIN Pledges 
-   ON Projects.id = Pledges.project_id;"
+   ON Projects.id = Pledges.project_id
+   GROUP BY Projects.category"
    
 end
 
